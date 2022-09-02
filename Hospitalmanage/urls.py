80477d896ff314b1cf723478f225797be1568e7f
amalnadh <amalnadhtm@gmail.com>
2022-09-02 20:31:35 +0530

    re_path url

--- Hospitalmanage/urls.py
+++ Hospitalmanage/urls.py
@@ -17,12 +17,12 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('home.urls')),
+    re_path('admin/', admin.site.urls),
+    re_path('',include('home.urls')),
 
 
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
